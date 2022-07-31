1743bbda39fcc1fe142780664f874d198ce7f755
chatonode <chatonode@gmail.com>
2022-07-31 20:17:08 +0100

    Initial 'contact' service setup (excluded NATS-related stuffs)

---
+++ contact/src/test/valid-id-generator.ts
@@ -0,0 +1,10 @@
+
+
+const getValidObjectId = () => {
+    // Generate a valid Mongoose ObjectId (as a String)
+    const id = '62e6cbd738a9ede39bdacc69'
+
+    return id
+}
+
+export { getValidObjectId }
