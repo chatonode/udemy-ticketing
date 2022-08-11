9b9af76576f5e53b7e825f65605e95a9aad34a08
chatonode <chatonode@gmail.com>
2022-08-11 02:07:19 +0100

    Added new properties to publishers, runned automated tests successfully. Updated common module.

--- auth/src/routes/__test__/sign-up.test.ts
+++ auth/src/routes/__test__/sign-up.test.ts
@@ -1,6 +1,12 @@
 import request from 'supertest'
 import { app } from '../../app'
 
+// It is STILL going to import fake natsWrapper
+import { natsWrapper } from '../../nats-wrapper'
+import { Subjects } from '@chato-zombilet/common'
+// Event Publisher Spy
+const natsWrapperSpy = jest.spyOn(natsWrapper.client, "publish")
+
 it('returns a 201 on successful signup', async () => {
     return request(app)
         .post('/api/users/signup')
@@ -76,4 +82,22 @@ it('sets a cookie after successful signup', async() => {
         .expect(201)
     
     expect(response.get('Set-Cookie')).toBeDefined()
+})
+
+it(`publishes a 'user:signed-up' event`, async () => {
+    // Test
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'Password123'
+        })
+        .expect(201)
+
+    // Event Assertion
+    // // Publisher Invoked: 'user-signed-up'
+    expect(natsWrapper.client.publish).toHaveBeenCalled()
+    // // Event Subject
+    const createdEventSubject = natsWrapperSpy.mock.calls[0][0]
+    expect(createdEventSubject).toEqual(Subjects.UserSignedUp)
 })
