db0e81a8226035a149f0043f7ef341725d4bcee5
chatonode <chatonode@gmail.com>
2022-09-28 20:08:38 +0100

    Added 'validate-token' router and its tests. Also new error type called TokenExpiredError at common module. Added one more date helper function called 'removeHoursFromDate' to test an expired token.

--- auth/src/models/token.ts
+++ auth/src/models/token.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-import jwt from 'jsonwebtoken'
+import { createHash } from 'crypto'
 
 import { UserDoc } from './user'
 
@@ -32,6 +32,7 @@ interface TokenDoc extends mongoose.Document {
 // An interface that describes the properties
 // that a Token Model has
 interface TokenModel extends mongoose.Model<TokenDoc> {
+    toHash(tokenValue: string): string
     build(attrs: TokenAttrs): TokenDoc
 }
 
@@ -62,7 +63,7 @@ const tokenSchema = new mongoose.Schema({
             delete ret._id
 
             // __v -> -
-            delete ret.__v   //Enabled for not using 'version'
+            delete ret.__v   //Disabled for not using 'version'
         }
     }
 })
@@ -72,11 +73,28 @@ const tokenSchema = new mongoose.Schema({
 */
 // Before Saving Token
 tokenSchema.pre('save', async function(done) {
+    if (this.isModified('value')) {
+        const hashed = Token.toHash(this.get('value'))
+
+        this.set('value', hashed)
+    }
 
     // Mandatory method call while using Async Mongoose
     done()
 
 })
+
+// Static Method: toHash
+tokenSchema.statics.toHash = (tokenValue: string) => {
+    // Generate a 'hash'
+    const hash = createHash('sha256')
+
+    // Update the 'hash' with the 'token'
+    hash.update(tokenValue)
+
+    // Return 'hashed token'
+    return hash.digest('hex')
+}
 
 // Any time we want to create a new token; we are not going to call 'new Token()' constructor
 // Instead, we are going to use this custom static function as below: 
